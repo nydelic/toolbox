2a7a60fb17848ecbd64d709e08a3748f944d8d43
josias-r <josias.ribi@gmail.com>
2022-06-28 19:52:36 +0200

    fix: add http handlers to index named exports

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ export { default as truncate } from "utils/truncate";
 
 // hooks
 export { default as useWindowSize } from "hooks/useWindowSize";
+
+// handlers
+export { default as HttpRequestError } from "handlers/http/HttpRequestError";
+export { default as httpResponse } from "handlers/http/httpResponse";
+export { default as httpErrorResponse } from "handlers/http/httpErrorResponse";
